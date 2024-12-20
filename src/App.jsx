b23340e2af7a2810b5678300c35acdaa0a124526
Ali khosronejad <khosronejadali344@gmail.com>
2024-12-20 13:33:05 +0330

    create Router and add homepage to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
+import Router from "./router";
 
 
 function App(){
   return(
     <>
-     
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
     </>
   );
+
 }
-export default App
+export default App;
