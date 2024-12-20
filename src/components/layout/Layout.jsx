ede435364072e5be2b96675e3ef221d894b360af
Ali khosronejad <khosronejadali344@gmail.com>
2024-12-20 13:35:42 +0330

    layout craeted

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,12 @@
+export default function Layout({children}){
+    return(
+        <>
+            <div>
+                {/* modals */}
+            </div>
+            <div>
+                {children}
+            </div>
+        </>
+    )
+}
