83687f623fc8f43887135ed6aa8b82c15ab791bc
Ali khosronejad <khosronejadali344@gmail.com>
2025-01-07 16:29:30 +0330

    customer services edited

--- src/components/footer/footerMenus/CustomerServices.jsx
+++ src/components/footer/footerMenus/CustomerServices.jsx
@@ -6,8 +6,11 @@ import FooterColTitle from "../FooterColTilte";
 export default function CustomerServices(){
     return(
         <div>
+
             <FooterColTitle title="خدمات مشتریان" />
+            
             <ul>
+
                 <ListItemLink href="#" linkText={"پاسخ به پرسشهای متداول"}/>
                 <ListItemLink href="#" linkText={"رویه‌های بازگردانی کالا"}/>
                 <ListItemLink href="#" linkText={"شرایط استفاده"}/>
