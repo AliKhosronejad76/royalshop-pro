e24bfe64f4f70802caf68739a1eff8f5a6d3ca45
Ali khosronejad <khosronejadali344@gmail.com>
2025-01-07 13:18:24 +0330

    add scrolltotop to router.jsx

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,8 +2,22 @@ import {Route , Routes} from "react-router-dom";
 import Home from "../pages/Home";
 import Search from "../pages/Search";
 import ProductDetails from "../pages/ProductDetails";
+import { useLocation } from "react-router-dom";
+
+
 
 export default function Router(){
+    const { pathname } = useLocation();
+
+  // Automatically scrolls to top whenever pathname changes
+   useEffect(() => {
+     window.scrollTo(0, 0);
+   }, [pathname]);
+   useEffect(()=>{
+        window.onbeforeunload = function () {
+            window.scrollTo(0, 0);
+        }
+    },[])
     return(
         <Routes>
             <Route index path="/" element={<Home/>}/>
